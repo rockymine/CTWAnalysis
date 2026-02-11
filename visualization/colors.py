7b0cd678d1c3574a2f7ab6f1ba4ffea133675c44
michael <michael.g186@live.de>
2026-02-11 12:20:59 +0100

    Add spawn colors for multiple teams

--- visualization/colors.py
+++ visualization/colors.py
@@ -10,6 +10,22 @@ NEUTRAL_COLOR = '#6b7280'
 # POI colors
 WOOL_COLOR = '#f1c40f'
 SPAWN_COLORS = {
-    'blue': '#3b82f6',
-    'red': '#ef4444',
+    'white': '#e9ecec',
+    'silver': '#8e8e86',
+    'orange': '#f07613',
+    'cyan': '#158991',
+    'magenta': '#bd44b3',
+    'purple': '#792aac',
+    'light blue': '#3aafd9',
+    'light_blue': '#3aafd9',
+    'light-blue': '#3aafd9',
+    'blue': '#35399d',
+    'yellow': '#f8c527',
+    'brown': '#724728',
+    'lime': '#70b919',
+    'green': '#546d1b',
+    'pink': '#ed8dac',
+    'red': '#a02722',
+    'gray': '#3e4447',
+    'black': '#141519'
 }
